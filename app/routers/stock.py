8ed376a2bbacf7e0438078f6f0ef5570b1a71580
saljeshh <saljesh2020@gmail.com>
2023-12-14 09:11:14 +0545

    latest algorithm updated

--- app/routers/stock.py
+++ app/routers/stock.py
@@ -28,6 +28,33 @@ def stock_data_according_to_category(stock, investable_amount):
 
     return stock_data
 
+def average_finder(stocks, sector):
+    total_roe = 0
+    total_pe = 0
+    total_pb = 0
+    total_roa = 0
+    count = 0
+
+    for stock in stocks:
+        if stock.sector == sector:
+            total_roe += stock.roe
+            total_pe += stock.pe
+            total_pb += stock.pb
+            total_roa += stock.roa
+            count += 1
+
+    if count == 0:
+        return 0, 0, 0,0  # Avoid division by zero if there are no stocks in the sector
+
+    avg_roe = round(total_roe / count, 2)
+    avg_pe = round(total_pe / count, 2)
+    avg_pb = round(total_pb / count, 2)
+    avg_roa = round(total_roa / count, 2)
+
+    return avg_roe, avg_pe, avg_pb, avg_roa
+
+
+
 
 # Function to filter and process the stock data
 def filter_and_process_stocks(stocks, id, db):
@@ -37,85 +64,129 @@ def filter_and_process_stocks(stocks, id, db):
 
     for stock in stocks:
         if stock.sector == "Commercial Banks":
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Commercial Banks')
             if (
-                (stock.roa > 1)
-                and (stock.roe > 12)
-                and (stock.pe < 14)
-                and (stock.pb < 2)
+
+                (stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)
             ):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Microfinance":
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Microfinance')
             if (
-                (stock.pe > 23 and stock.pe < 50)
-                and (stock.roe > 15)
-                and (stock.pb < 5)
+                (stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)
             ):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Development Banks":
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Development Banks')
             if (
-                (stock.pe > 10 and stock.pe < 30)
-                and (stock.roe > 10)
-                and (stock.pb < 3)
+                (stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)
             ):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Hydro Power":
-            if (stock.pe > 4 and stock.pe < 20) and (stock.pb < 5):
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Hydro Power')
+            if ((stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Manufacturing And Processing":
-            if stock.pe > 5 and stock.pe < 30:
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Manufacturing And Processing')
+            if ((stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Finance":
-            if (stock.pe > 10 and stock.pe < 30) and stock.roe > 10:
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Finance')
+            if ((stock.roe < avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb > avg_pb)
+                and (stock.roa < avg_roa)):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Life Insurance":
-            if stock.roe > 6:
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Life Insurance')
+            if((stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Non Life":
-            if stock.roe > 10 and stock.pb < 5:
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Non Life')
+            if ((stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Investment":
-            if stock.pe < 30:
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Investment')
+            if ((stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
-        if stock.sector == "Others" or stock.sector == "Tradings":
-            if stock.pe < 30:
+        if stock.sector == "Others":
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Others')
+            if ((stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)):
+                stock_data = stock_data_according_to_category(stock, investable_amount)
+                filtered_stocks.append(stock_data)
+        
+        if stock.sector == "Tradings":
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Tradings')
+            if ((stock.roe > avg_roe)
+                and (stock.pe > avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
         if stock.sector == "Hotels":
-            if stock.roe > 10 and stock.pe < 40:
+            avg_roe, avg_pe, avg_pb,avg_roa = average_finder(stocks, 'Hotels')
+            if ((stock.roe > avg_roe)
+                and (stock.pe < avg_pe)
+                and (stock.pb < avg_pb)
+                and (stock.roa > avg_roa)):
                 stock_data = stock_data_according_to_category(stock, investable_amount)
                 filtered_stocks.append(stock_data)
 
     return filtered_stocks
 
 
-def test(stocks):
-    print(stocks)
-
 
 @router.get("/")
 def get_stocks_recommended(
     current_user: int = Depends(oauth2.get_current_user),
     db: Session = Depends(get_db),
-    limit: int = 100,
+    limit: int = 200,
     search: Optional[str] = "",
 ):
     stocks = (
